6ecfe1332160266e2ab966b21ab096816d76b5e4
Felipe <felmartins1985@gmail.com>
2022-09-23 14:05:47 -0300

    feat: criado o PVE

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import Character from './Character';
+import Monster from './Monster';
+import Battle, { PVP, PVE } from './Battle';
+import Dragon from './Dragon';
+
+export const player1 = new Character('Felipe');
+export const player2 = new Character('Daniella');
+export const player3 = new Character('Guilherme');
+for (let i = 0; i < 50; i += 1) player1.levelUp();
+
+export const monster1 = new Monster();
+export const monster2 = new Dragon();
+export const pvp = new PVP(player2, player3);
+export const pve = new PVE(player1, [monster1, monster2]);
+export const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
